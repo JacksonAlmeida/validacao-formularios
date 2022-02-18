a430bdcd1c6fe0dff80b3df2f9c29029ad13ac93
Jackson Almeida <jacksonnalmeida30@gmail.com>
2022-02-18 17:40:39 -0400

    ponto um pouco de segurança no form

--- index.php
+++ index.php
@@ -9,31 +9,64 @@
 </head>
 
 <body>
-    <h1>Validando formularios</h1>
-    <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
-        Nome: <input type="text" name="nome" placeholder="Digite o seu nome completo">
-        <br><br>
-        Email: <input type="email" name="email" placeholder="exemplo@gmail.com">
-        <br><br>
-        <input type="submit">
-    </form>
+    <h1>PHP Validação de formulario exemplo</h1>
+
+    <fieldset>
+        <form method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <h3>Dados pessoais</h3>
+            <hr>
+            <p>Nome: <input type="text" name="nome" placeholder="Digite seu nome completo"></P>
+            <p><b>Genero:</b> <input type="radio" name="sexo" value="masculino" >masculino
+                <input type="radio" name="sexo" value="feminino">feminino
+                <input type="radio" name="sexo" value="outro">não quero informar
+            </p>
+            Tel:<input type="tel" name="telefone" placeholder="(xx) xxxxx-xxxx">
+            <p>E-mail: <input type="email" name="email" placeholder="exemplo@gmail.com"></p>
+            <p>Website: <input type="url" name="website" placeholder="https://exemplo.com.br"></p>
+
+            <h3>Comentário</h3>
+            <textarea name="comment" rows="10" cols="40">comment:</textarea>
+            <br>
+            <br>
+            <input type="submit">
+        </form>
+    </fieldset>
 
     <?php
+
+    $nome;
+    $genero;
+    $telefone;
+    $email;
+    $website;
+    $comment;
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nome = $_REQUEST['nome'];
-        $email = $_REQUEST['email'];
-        if (empty($nome) || empty($email)) {
-    ?>
-            <script>
-                alert("Ops! não pode deixar os campos do formulário vázio");
-            </script>
-    <?php
-        } else {
-            echo "<hr>nome:" . $nome;
-            echo "<br>email:" . $email;
-        }
+        $nome = test_input($_POST['nome']);
+        $genero = test_input($_POST['sexo']);
+        $telefone = test_input($_POST['telefone']);
+        $email = test_input($_POST['email']);
+        $website = test_input($_POST['website']);
+        $comment = test_input($_POST['comment']);
+
+        ?>
+
+        <div>
+            <h2>DADOS REGISTRADOS</h2>
+        </div>
+
+        <?php
+    }
+
+    function test_input($data)
+    {
+        $data = trim($data); //retira caracteres desnecessarios como, espaço extra, tabulação entre outros
+        $data = stripslashes($data); //remova barras invertidas;
+        $data = htmlspecialchars($data); //converte scripts em codigo entidade de html
+        return $data;
     }
     ?>
+
 </body>
 
 </html>
